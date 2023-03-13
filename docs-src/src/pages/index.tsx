6816e3e3ea093c18ab048694f8837771f6bf2e67
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-03-13 13:41:39 -0400

    update docs
    add vercel analytics

--- docs-src/src/pages/index.tsx
+++ docs-src/src/pages/index.tsx
@@ -6,6 +6,9 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
