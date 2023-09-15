d32b7a70ec70e9b1a281614784c55d7959251034
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-09-15 10:22:59 -0400

    update favicon

--- docs-src/docusaurus.config.js
+++ docs-src/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Lightning Vision",
   //   tagline: 'Dinosaurs are cool',
-  //   favicon: 'img/favicon.ico',
+    favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
   url: "https://visionlab-docs.vercel.app/",
